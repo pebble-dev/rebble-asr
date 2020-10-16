a185a031be5f1fdac615416ed1d8451014677ef1
Joshua Wise <joshua@joshuawise.com>
2020-10-16 01:14:55 -0400

    Don't you wish you had observability?  I do.  Debugging this would have been easier if I did.

--- asr/__init__.py
+++ asr/__init__.py
@@ -69,7 +69,7 @@ def recognise():
             'language_code': lang,
             'sample_rate_hertz': 16000,
             'max_alternatives': 1,
-            'enableAutomaticPunctuation': true,
+            'enableAutomaticPunctuation': True,
             # 'metadata': {
             #     'interaction_type': 'DICTATION',
             #     'microphone_distance': 'NEARFIELD',
